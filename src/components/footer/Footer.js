5b51fe31e1aebb8de9f89736feafad3cb7ab6a80
Dinu Vidrascu <96vlas@gmail.com>
2023-06-26 23:37:06 +0300

    fix box-shadow project box

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,31 +6,39 @@ import linkedin from "./../../img/icons/linkedin.svg"
 import "./style.css"
 
 function Footer() {
-   return (
-      <footer className="footer">
-        <div className="container">
-          <div className="footer__wrapper">
-            <ul className="social">
-              <li className="social__item">
-                <a href="https://www.facebook.com/dinu.vidrascu.29" target="_blank"><img src={facebook} alt="link"/></a>
-              </li>
-              <li className="social__item">
-                <a href="https://github.com/DinuVidrascu" target="_blank"><img src={github} alt="link"/></a>
-              </li>
-              <li className="social__item">
-                <a href="https://www.linkedin.com/in/dinu-vidrascu-a922b8226" target="_blank"><img src={linkedin} alt="link"/></a>
-              </li>
-              <li className="social__item">
-                <a href="https://www.instagram.com/dinu.vidrascu/" target="_blank"><img src={instagram} alt="link"/></a>
-              </li>
-            </ul>
-            <div className="copyright">
-              <p>Copyright © 2023. All rights are reserved</p>
-            </div>
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper">
+          <ul className="social">
+            <li className="social__item">
+              <a href="https://www.facebook.com/dinu.vidrascu.29" target="_blank">
+                <img src={facebook} alt="link" />
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="https://github.com/DinuVidrascu" target="_blank">
+                <img src={github} alt="link" />
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="https://www.linkedin.com/in/dinu-vidrascu-a922b8226" target="_blank">
+                <img src={linkedin} alt="link" />
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="https://www.instagram.com/dinu.vidrascu/" target="_blank">
+                <img src={instagram} alt="link" />
+              </a>
+            </li>
+          </ul>
+          <div className="copyright">
+            <p>Copyright © 2023. All rights are reserved</p>
           </div>
         </div>
-      </footer>
-   )
+      </div>
+    </footer>
+  )
 }
 
 export default Footer;
--- src/components/myproject/MyProject.js
+++ src/components/myproject/MyProject.js
@@ -7,7 +7,9 @@ function MyProject({ title, img, index }) {
    return (
       <NavLink to={`/project/${index}`}>
          <li className="project">
-            <img src={img} alt={title} className="project__img" />
+            <img src={img}
+               alt={title}
+               className="project__img" />
             <h3 className="project__title">{title}</h3>
          </li>
       </NavLink>
